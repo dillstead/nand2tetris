0f7ce0aa1731760207d2829e5bd76b70b57ffc53
Stedman Dillroper <dillstead@gmail.com>
2016-03-03 19:33:54 -0500

    Implement Mult.

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,49 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+    @R2
+    M=0
+    @R0
+    D=M
+    @R1
+    D=D-M
+    @R1_MIN
+    D;JLT
+    // R1 >= R0
+    @R1
+    D=M
+    @max
+    M=D
+    @R0
+    D=M
+    @min
+    M=D
+    @MULTIPLY
+    0;JMP
+    (R1_MIN)
+    // R1 < R0
+    @R1
+    D=M
+    @min
+    M=D
+    @R0
+    D=M
+    @max
+    M=D
+    (MULTIPLY)
+    // Add max value to itself min times.
+    @min
+    D=M
+    @END
+    D;JEQ
+    @max
+    D=M
+    @R2
+    M=D+M
+    @min
+    M=M-1
+    @MULTIPLY
+    0;JMP
+    (END)
+    @END
+    0;JMP
