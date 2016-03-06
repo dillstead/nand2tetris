e807cfd25b0ce0d567b24d559913c5a1b7dcf40a
Stedman Dillroper <dillstead@gmail.com>
2016-03-05 20:49:03 -0500

    Implement Fill.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -9,3 +9,52 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+    // screen_end is set to final screen memory location.
+    @SCREEN
+    D=A
+    @8192
+    D=D+A
+    @screen_end
+    M=D    
+    (START)
+    // screen_start is initialized to the first screen memory location.
+    @SCREEN
+    D=A
+    @screen_current
+    M=D
+    // Read a key from the keyboard.
+    @KBD
+    D=M
+    @KEY_PRESSED
+    D;JNE
+    // Keyboard reads 0, key not pressed, set color to clear.
+    @color
+    M=0
+    @FILL
+    0;JMP
+    (KEY_PRESSED)
+    // Keyboard reads non-0, key pressed, set color to black.
+    @color
+    M=-1
+    (FILL)
+    // Write color to the current screen location.
+    @color
+    D=M
+    @screen_current
+    A=M
+    M=D
+    // Update screen_current to the next memory location.
+    @screen_current
+    MD=M+1
+    // If the final location is reached, listen for another keypress, else contine to
+    // fill.
+    @screen_end
+    D=D-M
+    @START
+    D;JEQ
+    @FILL
+    0;JMP
+    // Never reached.
+    (END)
+    @END
+    0;JMP
